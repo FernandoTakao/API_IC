5b505ad57ecc0d02c718fc47e05aad3a3836a7ca
FernandoTakao <fernandotakaowatanabe@gmail.com>
2026-04-29 14:30:06 -0300

    commit teste

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/meu-banco')
+mongoose.connect('mongodb://127.0.0.1:27017/outroBanco')
 .then(() => console.log('MongoDB conectado'))
 .catch(err => console.log('Erro ao conectar:', err));
 
