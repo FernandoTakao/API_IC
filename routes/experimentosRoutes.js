f4adb38265e47ae2232cacd2e62f15bd9ffe6c9e
FernandoTakao <fernandotakaowatanabe@gmail.com>
2026-06-19 18:07:15 -0300

    experimentController function createExperiment apagado do route

--- routes/experimentosRoutes.js
+++ routes/experimentosRoutes.js
@@ -4,11 +4,10 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 const experimentoController = require("../controllers/experimentoController");
 
-router.post("/", auth, experimentoController.createExperimento);
 router.post("/chaves",auth, experimentoController.generateExperimentKey);
 router.get("/meus-experimentos", auth, experimentoController.getMyExperimentos);
 router.get("/:id/colunas", auth, experimentoController.getExperimentoColunas);
-router.get("/:id", auth, experimentoController.getExperimentoByKey);
+router.get("/:id", auth, experimentoController.getExperimentoById);
 router.patch("/:id", auth, experimentoController.updateExperimento);
 router.delete("/:id", auth, experimentoController.deleteExperimento);
 
